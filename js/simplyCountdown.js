2a57fb97350c389dd089d5fc8b3e309704f564bb
williamorim <thewilliam89@gmail.com>
2025-07-16 08:43:12 -0300

    atualizando 2025

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,10 +115,10 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2023,
-                month: 6,
-                day: 24,
-                hours: 10,
+                year: 2025,
+                month: 7,
+                day: 26,
+                hours: 15,
                 minutes: 0,
                 seconds: 0,
                 words: {
